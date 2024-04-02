3a3c12258a036bfa2de149ddc4886d3b93c37a00
Jack Meyer <jaakmyr@gmail.com>
2024-04-01 17:42:30 -0700

    now pokemon data is stored in local storage, so it persists across refreshes!

--- js/main.js
+++ js/main.js
@@ -1,97 +1,8 @@
-//Example fetch using pokemonapi.co
-document.querySelectorAll('button').forEach(button => {
-    button.addEventListener('click', e => {
-        placeAfterRating(e)
-        showNewImage()
-    })
-})
-
-if(!localStorage.getItem('seenPokemon')){ //if theres no local storage create it
-    localStorage.setItem('seenPokemon', JSON.stringify([]))
-}
-
-//when page is loaded load the first pokemon on deck:
-//will be in localstorage but still pure random for now
-showNewImage()
-
-
-function showNewImage(){ //determine where last poekmon goes... then show new one
-  //const choice = document.querySelector('input').value.toLowerCase()
-  let max = 151
-  let choice = 0
-  while (true){
-    choice = getRandomInt(1,max);
-    if (!getSeenPokemonList().includes(choice)){
-        break
-    }
-  }
- 
-  addIdToLocalStorage(choice)
-  if (getLocalStorageLength('seenPokemon') == max){
-    alert("FINISHED! CLEARING LOCAL STORAGE")
-    localStorage.clear()
-  }
-
-
-  const url = 'https://pokeapi.co/api/v2/pokemon/'+choice
-
-  fetch(url)
-      .then(res => res.json()) // parse response as JSON
-      .then(data => {
-        imageSrc = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default']
-        pokeName = capitalizeFirstLetter(data['name'])
-
-        document.querySelector('#mainPhoto').src = imageSrc
-        document.querySelector('#mainPokeName').textContent = pokeName
-      })
-      .catch(err => {
-          console.log(`error ${err}`)
-      });
-}
-
-function placeAfterRating(e){
-    let parent = document.querySelector('#'+e.target.id+'ed') //ghetto ass shit right here
-    let imageSrc = document.querySelector("#mainPhoto").src
-    let pokeName = document.querySelector("#mainPokeName").textContent
-    //create pokemon in collumn
-    createnNewPokemonInColumn(imageSrc, pokeName, parent)
-}
-
-function capitalizeFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
-}
-
-function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-
-function addIdToLocalStorage(id){ //push new id to local storage
-    let list = JSON.parse(localStorage.getItem('seenPokemon'))
-    list.push(id)
-    localStorage.setItem('seenPokemon', JSON.stringify(list))
-}
-
-function getSeenPokemonList(){ //returns the get pokemon list from local storage
-    return JSON.parse(localStorage.getItem('seenPokemon'))
-}
-
-function getLocalStorageLength(key){
-    return JSON.parse(localStorage.getItem(key)).length
-}
-
-function createnNewPokemonInColumn(src, pokeName, parent){
-    let newPIC = new PokemonInColumn(src, pokeName, parent)
-    newPIC.render()
-
-}
-
 class PokemonInColumn{
-    constructor(src='#', name='blank-pokemon', parent=Node){ //gets name and image of pokemon
+    constructor(src='#', name='blank-pokemon', parentID='#none'){ //gets name and image of pokemon
         this.src = src
         this.name = name
-        this.parent = parent //dom parent being appended to
+        this.parentID = parentID //dom parent being appended to
     }
     render() {
         const card = document.createElement('div');
@@ -105,6 +16,128 @@ class PokemonInColumn{
         heading.textContent = this.name;
         card.appendChild(heading);
 
-        this.parent.appendChild(card);
+        document.querySelector('.'+this.parentID).appendChild(card);
     }
+}
+
+//Example fetch using pokemonapi.co
+document.querySelectorAll('button').forEach(button => {
+    button.addEventListener('click', e => {
+        placeAfterRating(e)
+        showNewImage()
+    })
+})
+
+if(!localStorage.getItem('pokemonLocalStorage')){ //if theres no local storage create it
+    localStorage.setItem('pokemonLocalStorage', JSON.stringify({'seenPokemon': [], 'smashedPokemon': [], 'passedPokemon': [], 'mainSectionPokemon':''}))
+} // 0 is seen pokemon, 1 is smahsed, 2 is passed
+
+//when page is loaded load the first pokemon on deck:
+//will be in localstorage but still pure random for now
+
+startCall()
+
+function showNewImage(){ //determine where last poekmon goes... then show new one
+  //const choice = document.querySelector('input').value.toLowerCase()
+  let max = 151
+  let choice = getRandomPokeNumber(max)
+  
+ 
+  addElementToLocalStorage(choice, 'seenPokemon') //adds choice to seen pokemon
+  if (getLocalStorageLength('pokemonLocalStorage') == max){
+    alert("FINISHED! CLEARING LOCAL STORAGE")
+    localStorage.clear()
+  }
+
+
+  const url = 'https://pokeapi.co/api/v2/pokemon/'+choice
+
+  fetch(url)
+      .then(res => res.json()) // parse response as JSON
+      .then(data => {
+        imageSrc = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default']
+        pokeName = capitalizeFirstLetter(data['name'])
+
+        pokeCard = new PokemonInColumn(imageSrc, pokeName, 'mainSection')
+        pokeCard.render()
+        addElementToLocalStorage(pokeCard, 'mainSectionPokemon') //could be just url
+      })
+      .catch(err => {
+          console.log(`error ${err}`)
+      });
+}
+
+function placeAfterRating(e){
+    let newParentID = e.target.id+'ed' //ghetto ass shit right here
+
+    newPokemon = getLocalStorageList('mainSectionPokemon')
+    newPokemon.parentID = newParentID
+
+    pokeCard = document.querySelector('.mainSection').querySelector('.card')
+    document.querySelector('.'+newPokemon.parentID).appendChild(pokeCard);
+
+    addElementToLocalStorage(newPokemon, newPokemon.parentID+"Pokemon")
+} //DO THIS ON IMAGE CREATION!!! MAKE BIG ONE SMALL ONE? WE WILL SEe
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+function addElementToLocalStorage(element, subIndex){ //push new id to local storage
+    let value = JSON.parse(localStorage.getItem('pokemonLocalStorage'))
+    if (Array.isArray(value[subIndex])){ //if array or just one
+        value[subIndex].push(element)
+       
+    }else{
+        value[subIndex] = element
+    }
+    localStorage.setItem('pokemonLocalStorage', JSON.stringify(value))
+}
+
+function getLocalStorageList(listIndex){ //returns the get pokemon list from local storage
+    return JSON.parse(localStorage.getItem('pokemonLocalStorage'))[listIndex]
+}
+
+function getLocalStorageLength(key){
+    return JSON.parse(localStorage.getItem(key)).length
+}
+
+function startCall(){
+    if(getLocalStorageList('mainSectionPokemon') == ''){
+        showNewImage()
+    }
+    else{
+        mainPokemon = getLocalStorageList('mainSectionPokemon')
+        newCard = new PokemonInColumn(mainPokemon.src, mainPokemon.name, mainPokemon.parentID) //CHAT ILL FIX IT I SWEAR IM SORRY
+        newCard.render()
+    }
+    
+    
+
+    getLocalStorageList('smashedPokemon').forEach(card =>{
+        newCard = new PokemonInColumn(card.src, card.name, card.parentID)
+        newCard.render()
+    })
+    getLocalStorageList('passedPokemon').forEach(card =>{
+        newCard = new PokemonInColumn(card.src, card.name, card.parentID)
+        newCard.render()
+    })
+    
+}
+
+function getRandomPokeNumber(max){
+    let choice
+    while (true){
+        choice = getRandomInt(1,max);
+        if (!getLocalStorageList('seenPokemon').includes(choice)){
+            break
+        }
+      }
+    return choice
 }
